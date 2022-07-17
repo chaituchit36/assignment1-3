7e63f883b6c5b9089819892839f5c65441b5fc75
chaituchit36 <107355831+chaituchit36@users.noreply.github.com>
2022-07-17 23:01:56 +0530

    Add files via upload

---
+++ assign3.py
@@ -0,0 +1,10 @@
+lst=[28,29,3,5,22]
+even=0
+odd=0
+for i in lst:
+    if(i%2==0):
+        even=even+1
+    else:
+            odd=odd+1
+print("even numbers in lst:",even)
+print("odd numbers in lst:",odd)            
